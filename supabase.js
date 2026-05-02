0fdc19c597dbd382501292398ea751c35ae34706
Damian396 <83165373+Damian396@users.noreply.github.com>
2026-05-02 18:52:27 +0200

    Notify if Supabase script is not loaded
    Added alert for Supabase script loading failure.

--- supabase.js
+++ supabase.js
@@ -1,6 +1,11 @@
 if (!window.supabaseClient) {
+
   const supabaseUrl = "https://gzhapafmlnefhnrpehiz.supabase.co";
   const supabaseKey = "sb_publishable_9s7jzuXbv4UrHBp-Yl6FHQ_04AFb07H";
+
+  if (!window.supabase) {
+    alert("Supabase Script nicht geladen!");
+  }
 
   window.supabaseClient = window.supabase.createClient(
     supabaseUrl,
