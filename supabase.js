87a7a02c4b1ce53dc27988d5ce84eb36a72d41ae
Damian396 <83165373+Damian396@users.noreply.github.com>
2026-05-02 15:12:23 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,9 @@
-const supabaseUrl = "https://gzhapafmlnefhnrpehiz.supabase.co";
-const supabaseKey = "sb_publishable_9s7jzuXbv4UrHBp-Yl6FHQ_04AFb07H";
+if (!window.supabaseClient) {
+  const supabaseUrl = "https://gzhapafmlnefhnrpehiz.supabase.co";
+  const supabaseKey = "sb_publishable_9s7jzuXbv4UrHBp-Yl6FHQ_04AFb07H";
 
-const supabase = window.supabase.createClient(
-  supabaseUrl,
-  supabaseKey
-);
+  window.supabaseClient = window.supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+  );
+}
